4d7779ac9bb443db496ad014d2fbc43d502f1c57
Irina Tretiakova <iri54696376@gmail.com>
2023-01-27 16:21:30 +0100

    Checkbox, radiobutton, math calc

---
+++ lesson2.1_step5.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+link = "https://suninjuly.github.io/math.html"
+
+try: 
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+
+    # Ваш код, который заполняет обязательные поля
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+    checkbox = browser.find_element(By.ID, "robotCheckbox")
+    checkbox.click()
+
+    radiobutton = browser.find_element(By.ID, "robotsRule")
+    radiobutton.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
