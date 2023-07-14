4f429b7d43f59dfaf5d83532ad27b772c3c68250
Irina Tretiakova <iri54696376@gmail.com>
2023-07-14 19:26:01 +0200

    Test runs with conftest.py file with fixture in it, for checking that parametr from command string was transferred to Conftest file

---
+++ test_parser.py
@@ -0,0 +1,12 @@
+import pytest
+import conftest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
